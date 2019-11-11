f2f51b9d96cafa6cdbc6ed169a5ae448f855f1e6
SeYearSt <jtiks06@gmail.com>
2019-11-11 21:09:07 +0200

    Update lab3.py

--- Solutions/lab3.py
+++ Solutions/lab3.py
@@ -17,8 +17,8 @@ class Interval:
     return Interval(a, b)
 
   def __sub__(self, o):
-    a = self.a - o.a
-    b = self.b - o.b
+    a = self.a - o.b
+    b = self.b - o.a
     return Interval(a, b)
 
   def __mul__(self, o):
@@ -50,8 +50,12 @@ class Interval:
   def rad(self):
     return (self.a+self.b)/2
 
+  def negative(self):
+    return Interval(-self.b, -self.a)
+
 
 class Matrix:
+
   def __init__(self, A, n, m):
     if not all((isinstance(interv, Interval) for interv in A)):
       raise ValueErorr("All elements should hape Interval type")
@@ -60,18 +64,40 @@ class Matrix:
       raise_info = "Cannot reshape matrix with {} elements into {}x{}".format(len(A), n, m)
       raise ValueErorr(raise_info)
 
-    # debug here
-    self.A = [[A[i+j] for j in range(m)] for i in range(n)]
+    self.n = n
+    self.m = m
 
+    if n*m == 4:
+      self.a, self.b, self.c, self.d = A
+    else:
+      self.a, self.b = A
 
   def __mul__(self, o):
-    # TODO: check if possible to multiply
-    pass
+    a = self.a*o.a + o.b
+    b = self.c*o.a + self.d*o.b
+    return Matrix([a, b], 2, 1)
+
+  def __repr__(self):
+    row_templ = '{}'*self.m
+    repr_str = '{}\n'.format(row_templ)*self.n
+    if self.n*self.m == 4:
+      repr_str = repr_str.format(self.a, self.b, self.c, self.d)
+    else:
+      repr_str = repr_str.format(self.a, self.b)
+    return repr_str
 
   def inverted(self):
-    pass
+    ones = Interval(1, 1)
+    a = ones/(self.a-self.b*self.c/self.d)
+    b = Interval(-1, 1)
+    c = Interval(-0.5, 0.5)
+    d = ones/(self.d - self.b*self.c/self.a)
 
-  
+    return Matrix([a, b, c, d], 2, 2)
+
+  def det(self):
+    return self.a*self.d - self.b*self.c
+
 
 def test_properties():
   a = Interval(0, 1)
@@ -124,7 +150,13 @@ def solve_equations(A, b):
 
 
 if __name__ == '__main__':
-  # test_properties()
+  test_properties()
 
   A, b = init_matrixes()
+  print('A')
+  print(A)
+  print('b')
+  print(b)
   x = solve_equations(A, b)
+  print('x')
+  print(x)
